3a8da7e565a1aefed47890cff4ab96265dc8a9ba
harvey <harvey.bae@kakaoenterprise.com>
2020-03-24 16:14:18 +0900

    axios requests for screen "deploy" implemented

--- src/views/SurveyDeploy.vue
+++ src/views/SurveyDeploy.vue
@@ -9,11 +9,11 @@
 						<template v-slot:activator="{ on }">
 							<v-icon dark v-on="on">info</v-icon>
 						</template>
-						<span>설문을 개시하려면 우측 "배포하기" 버튼을 클릭하세요<br>배포 후 종료하면 설문이 종료됩니다</span>
+						<span>설문을 개시하려면 우측 "배포하기" 버튼을 클릭하세요<br>배포 후 클릭하면 설문이 종료됩니다</span>
 					</v-tooltip>
 				</v-col>
 				<v-col cols="7">
-					<v-btn v-on:click="onClickDeploy" large color="accent">{{!open ? "배포하기" : "설문 진행중"}}</v-btn> 
+					<v-btn :disabled="status == 3 ? true : false" v-on:click="onClickDeploy" large color="accent">{{status === 1 ? "배포하기" : (status === 2 ? "설문 진행중" : "설문 종료")}}</v-btn> 
 				</v-col>
 			</v-row>
 			<v-row>
@@ -28,7 +28,7 @@
 				</v-col>
 				<v-col cols="7">
 					<v-textarea solo v-model="welcomeMsg"></v-textarea>
-					<v-btn v-on:click="onClickSaveSurveyMsg('welcome')" color="darkPrimary" class="saveBtn" width="100"><span class="btnSpan">저장</span></v-btn>
+					<v-btn :disabled="status == 3 ? true : false" v-on:click="onClickSaveSurveyMsg('welcome')" color="darkPrimary" class="saveBtn" width="100"><span class="btnSpan">저장</span></v-btn>
 				</v-col>
 			</v-row>
 			<v-row>
@@ -41,9 +41,9 @@
 						<span>설문 종료 시 응답자에게 노출되는 메시지를 설정해주세요</span>
 					</v-tooltip>
 				</v-col>
-				<v-col cols="7">
+				<v-col cols="7"> 
 					<v-textarea solo v-model="completeMsg"></v-textarea>
-					<v-btn v-on:click="onClickSaveSurveyMsg('complete')" color="darkPrimary" class="saveBtn" width="100"><span class="btnSpan">저장</span></v-btn>
+					<v-btn :disabled="status == 3 ? true : false" v-on:click="onClickSaveSurveyMsg('complete')" color="darkPrimary" class="saveBtn" width="100"><span class="btnSpan">저장</span></v-btn>
 				</v-col>
 			</v-row>
 		</v-container>
@@ -55,35 +55,36 @@
 		name: 'SurveyDeploy',
 		components: {},
 		data: () => ({
-			surveyId: 34,
-			welcomeMsg: '',
-			completeMsg: '',
-			open: false
+			surveyId: 1,
+			welcomeMsg: "",
+			completeMsg: "",
+			status: 1
 		}),
 		methods: {
-			created: function () {
-				const axios = require('axios');
-				console.log(this.surveyId);
-
-				axios.get('/api/getSurveyInfo')
-				.then(res => {
-					console.log(res);
-					this.welcomeMsg = res.welcomeMsg;
-					this.completeMsg = res.completeMsg;
-					this.open = res.open;
-				})
-			},
 			onClickDeploy: function () {
 				const axios = require('axios');
 
-				const inputSurveyId = this.$data.surveyId;
-				const inputOpen = !this.$data.open;
+				const inputSurveyId = this.surveyId;
+				const inputStatus = this.status + 1;
 
-				axios.post('/api/openSurvey', {
+				axios.post('http://localhost:8081/api/openSurvey', {
 					surveyId: inputSurveyId,
-					open: inputOpen
+					status: inputStatus
 				}).then(res => {
 					console.log(res);
+
+					if (res.data.result === 'true') {
+
+						if (this.status === 2) {
+							this.status = 3;
+							alert('설문이 정상적으로 종료되었습니다!');
+						} else {
+							this.status = 2;
+							alert('설문이 정상적으로 배포되었습니다!');
+						}
+					} else {
+						alert('배포 과정에 오류가 발생했습니다. 다시 시도해주세요.');
+					}
 				})
 			},
 			onClickSaveSurveyMsg: function (flag) {
@@ -94,23 +95,49 @@
 				const inputCompleteMsg = this.$data.completeMsg;
 
 				if (flag === 'welcome') {
-					axios.post('/api/setSurveyInfo', {
+					axios.post('http://localhost:8081/api/setSurveyInfo', {
 					surveyId: inputSurveyId,
-					welcomeMsg: inputWelcomeMsg
+					welcomeMsg: inputWelcomeMsg,
+					completeMsg: this.completeMsg
 				})
 				.then(res => {
 					console.log(res);
+					alert("요청이 정상적으로 수행되었습니다!");
 				})
 				} else {
-					axios.post('/api/setSurveyInfo', {
+					axios.post('http://localhost:8081/api/setSurveyInfo', {
 					surveyId: inputSurveyId,
+					welcomeMsg: this.welcomeMsg,
 					completeMsg: inputCompleteMsg
 				}).then(res => {
 					console.log(res);
+					alert("요청이 정상적으로 수행되었습니다!");
 				})
 				}
 			}
-		}
+		},
+		created: function () {
+				const axios = require('axios');
+
+				axios.get('http://localhost:8081/api/getSurveyInfo?surveyId=' + this.surveyId)
+				.then(res => {
+					console.log(res);
+					let welcomeMsg = res.data.welcomeMsg;
+					let completeMsg = res.data.completeMsg;
+
+					if (welcomeMsg === '') {
+						welcomeMsg = "아직 설정된 환영 메시지가 없습니다.";
+					}
+
+					if (completeMsg === '') {
+						completeMsg = "아직 설정된 종료 메시지가 없습니다.";
+					}
+
+					this.welcomeMsg = welcomeMsg;
+					this.completeMsg = completeMsg;
+					this.status = res.data.status;
+				})
+			}
 	}
 </script>
 
