ae2445a712dcc1ef2bce89f465fabd50a9366613
harvey <harvey.bae@kakaoenterprise.com>
2020-03-24 14:50:54 +0900

    axios get request for "getSurveyStatus" implemented

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -29,56 +29,14 @@
 		name: 'Status',
 		components: {},
 		data: () => ({
-			statusList: [
-				{ 
-					surveyId: 1,
-					surveyTitle: "KEP 설문봇 수요조사", 
-					count: 10,
-					startDate: "2020.03.23 13:56:02",
-					endDate: "2020.03.25 13:56:02"
-				},
-				{ 
-					surveyId: 2,
-					surveyTitle: "오픈 API 문서화 툴 사용 실태 조사", 
-					count: 1,
-					startDate: "2020.03.13 13:56:02",
-					endDate: "2020.03.14 13:56:02"
-				},
-				{ 
-					surveyId: 3,
-					surveyTitle: "사내 근태관리 솔루션 만족도 조사", 
-					count: 5,
-					startDate: "2020.03.23 13:56:02",
-					endDate: "2020.03.25 13:56:02"
-				},
-				{ 
-					surveyId: 4,
-					surveyTitle: "오피스 출근 수단 조사", 
-					count: 8,
-					startDate: "2020.03.23 13:56:02",
-					endDate: "2020.03.25 13:56:02"
-				},
-				{ 
-					surveyId: 5,
-					surveyTitle: "원격근무 만족도 조사", 
-					count: 20,
-					startDate: "2020.03.23 13:56:02",
-					endDate: "2020.03.25 13:56:02"
-				},
-				{ 
-					surveyId: 6,
-					surveyTitle: "카카오워크 베타 테스트 만족도 조사", 
-					count: 100,
-					startDate: "2020.02.23 13:56:02",
-					endDate: ""
-				}
-			],
+			registerId: 1,
+			statusList: [],
 			headers: [
 				{
 					text: '설문명',
 					align: 'start',
 					sortable: false,
-					value: 'surveyTitle',
+					value: 'surveyName',
 				},
 				{ text: '응답수', value: 'count' },
 				{ text: '시작 일시', value: 'startDate' },
@@ -87,16 +45,20 @@
 			]
 		}),
 		methods: {
-			created: function () {
+			
+		},
+		created: function () {
 				const axios = require('axios');
 				
-				axios.get('http://localhost:8080/api/getSurveyStatus')
+				axios.get('http://localhost:8081/api/getSurveyStatus?registerId=' + this.registerId)
 				.then(res => {
 					console.log(res);
-					this.statusList = res.statusList;
-				}) 
+					this.statusList = res.data.statusList;
+				})
+				.catch(err => {
+					console.log(err);
+				})
 			}
-		}
 	}
 </script>
 
