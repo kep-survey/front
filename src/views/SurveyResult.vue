552525cc9c14b343c2ef5c7ba788a7ffa1b65593
harvey <harvey.bae@kakaoenterprise.com>
2020-03-31 11:39:27 +0900

    add tab element

--- src/views/SurveyResult.vue
+++ src/views/SurveyResult.vue
@@ -3,41 +3,64 @@
 		<v-container class="mb-8">
 			<h1><span class="font-weight-light">결과</span>조회</h1>
 			<h1 class="survey-title mb-8" style="margin-top: 2rem;">{{surveyTitle}}</h1>
-			<v-card>
-				<v-card-title>
-					<v-text-field
-						v-model="search"
-						append-icon="mdi-magnify"
-						label="Search"
-						single-line
-						hide-details
-						color="teritory"
-					></v-text-field>
-				</v-card-title>
-				<v-data-table
-					:headers="headers"
-					:items="resultList"
-					:items-per-page="10"
-					:footer-props="{
-						showFirstLastPage: true,
-						firstIcon: 'mdi-arrow-collapse-left',
-						lastIcon: 'mdi-arrow-collapse-right',
-						prevIcon: 'mdi-minus',
-						nextIcon: 'mdi-plus'
-					}"
-					:sort-by="['participationTime']"
-					:sort-desc="[true]"
-					:search="search"
-					item-key="botUserId"
-					loading 
-					loading-text="데이터를 불러오는 중입니다..."
-					class="elevation-1"
+			<v-tabs
+				v-model="tab"
+				background-color="trapsparent"
+				class="elevation-1"
+				color="dark-primary"
+			>
+				<v-tabs-slider></v-tabs-slider>
+				<v-tab
+				:href="`#tab-1`"
 				>
-					<template v-slot:item.actions="{ item }">
-						<v-chip label color="accent" text-color="black" small :to="'/survey/detail/' + surveyId + '/' + item.botUserId">이동</v-chip>
-					</template>
-				</v-data-table>
-			</v-card>
+				응답 조회
+				</v-tab>
+				<v-tab
+				:href="`#tab-2`"
+				>
+				결과 분석
+				</v-tab>
+
+				<v-tab-item
+				:value="'tab-1'"
+				>
+				<v-card>
+					<v-card-title>
+						<v-text-field
+							v-model="search"
+							append-icon="mdi-magnify"
+							label="Search"
+							single-line
+							hide-details
+							color="teritory"
+						></v-text-field>
+					</v-card-title>
+					<v-data-table
+						:headers="headers"
+						:items="resultList"
+						:items-per-page="10"
+						:footer-props="{
+							showFirstLastPage: true,
+							firstIcon: 'mdi-arrow-collapse-left',
+							lastIcon: 'mdi-arrow-collapse-right',
+							prevIcon: 'mdi-minus',
+							nextIcon: 'mdi-plus'
+						}"
+						:sort-by="['participationTime']"
+						:sort-desc="[true]"
+						:search="search"
+						item-key="botUserId"
+						loading 
+						loading-text="데이터를 불러오는 중입니다..."
+					>
+						<template v-slot:item.actions="{ item }">
+							<v-chip label color="accent" text-color="black" small :to="'/survey/detail/' + surveyId + '/' + item.botUserId">이동</v-chip>
+						</template>
+					</v-data-table>
+				</v-card>
+				</v-tab-item>
+			</v-tabs>			
+			
 		</v-container>
 	</div>
 </template>
@@ -47,6 +70,7 @@
 		name: 'SurveyResult',
 		components: {},
 		data: () => ({
+			tab: null,
 			search: "",
 			surveyId: 0,
 			surveyTitle: "",
